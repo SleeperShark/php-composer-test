cb645057dc3c1d266772099d582eae215a0b4c6c
SleeperShark <40243105s@gmail.com>
2022-08-17 17:11:41 +0800

    Class Status Session

--- src/app/PaymentGateway/Paddle/Transaction.php
+++ src/app/PaymentGateway/Paddle/Transaction.php
@@ -3,14 +3,22 @@
 declare(strict_types=1);
 
 namespace App\PaymentGateway\Paddle;
+use App\Enums\Status;
 
 class Transaction{
-    public const STATUS_PAID = 'paid';
-    public const STATUS_PENDING = 'pending';
-    public const STATUS_DECLINED = 'declined';
+    private string $status;
 
     public function __construct(){
+        $this->setStatus(Status::PENDING);
+    }
+
+    public function setStatus(string $status): self{
+        if(!isset(Status::ALL_STATUSES[$status])){
+            throw new \InvalidArgumentException('Invalid status');
+        }
         
+        $this->status = $status;
+        return $this;
     }
 }
 
--- src/public/index.php
+++ src/public/index.php
@@ -1,7 +1,10 @@
 <?php
 declare(strict_types = 1);
 
+use App\Enums\Status;
 use App\PaymentGateway\Paddle\Transaction;
+
 require __DIR__.'/../vendor/autoload.php';
 
-echo Transaction::STATUS_PAID;
+$transaction = new Transaction();
+$transaction->setStatus(Status::PAID);
