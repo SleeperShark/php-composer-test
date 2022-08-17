eae469be76d1ec452b60f9033d0031f2819bd3ef
SleeperShark <40243105s@gmail.com>
2022-08-17 15:43:17 +0800

    Use autoload of composer

--- src/public/index.php
+++ src/public/index.php
@@ -2,13 +2,15 @@
 
 declare(strict_types = 1);
 
-spl_autoload_register(function($class){
-    $path = lcfirst(__DIR__.'/../'.str_replace('\\', '/', $class).'.php');
+// spl_autoload_register(function($class){
+//     $path = lcfirst(__DIR__.'/../'.str_replace('\\', '/', $class).'.php');
 
-    if(file_exists($path)){
-        require_once $class;
-    }
-});
+//     if(file_exists($path)){
+//         require_once $class;
+//     }
+// });
+
+require __DIR__.'/../vendor/autoload.php';
 
 use App\PaymentGateway\Paddle\Transaction;
 
