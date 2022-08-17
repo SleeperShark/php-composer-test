0aa399d5d243b406844821e74d06fca2b033778c
SleeperShark <40243105s@gmail.com>
2022-08-17 16:40:48 +0800

    Test: class constant

--- src/public/index.php
+++ src/public/index.php
@@ -1,21 +1,7 @@
 <?php
-
 declare(strict_types = 1);
 
-// spl_autoload_register(function($class){
-//     $path = lcfirst(__DIR__.'/../'.str_replace('\\', '/', $class).'.php');
-
-//     if(file_exists($path)){
-//         require_once $class;
-//     }
-// });
-
+use App\PaymentGateway\Paddle\Transaction;
 require __DIR__.'/../vendor/autoload.php';
 
-use App\PaymentGateway\Paddle\Transaction;
-
-$paddleTransaction = new Transaction();
-var_dump($paddleTransaction);
-
-$id = new \Ramsey\Uuid\UuidFactory();
-echo $id->uuid4();
+echo Transaction::STATUS_PAID;
