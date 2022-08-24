f6c1deb28524fe25a48e3ca15ecc4c3821dec852
SleeperShark <40243105s@gmail.com>
2022-08-24 12:15:43 +0800

    Refactor Router to class

---
+++ src/app/Classes/Invoice.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Classes;
+
+class Invoice {
+    public function index():string{
+        return 'INVOICES';
+    }
+
+    public function create():string{
+        return 'Create Invoice';
+    }
+}
--- src/public/index.php
+++ src/public/index.php
@@ -2,18 +2,10 @@
 
 require_once __DIR__.'/../vendor/autoload.php';
 
-echo '<pre>';
-print_r($_SERVER);
-echo '</pre>';
-
 $router = new App\Router();
 
-$router->register('/', function(){
-    echo 'HOME';
-});
-
-$router->register('/invoices', function(){
-    echo 'INVOICES';
-});
+$router->register('/', [App\Classes\Home::class, 'index'])
+       ->register('/invoices', [App\Classes\Invoice::class, 'index'])
+       ->register('/invoices/create', [App\Classes\Invoice::class, 'create']);
 
 echo $router->resolve($_SERVER['REQUEST_URI']);
