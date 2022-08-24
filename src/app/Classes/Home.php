ca49a5714a5a6a74281a286dd3bb3aa162ab776c
SleeperShark <40243105s@gmail.com>
2022-08-24 12:23:35 +0800

    Refactor resolve method

--- src/app/Classes/Home.php
+++ src/app/Classes/Home.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace App\Classes;
 
 class Home {
-    public static function index(){
+    public function index(){
         return 'Home';
     }
 }
--- src/app/Classes/Invoice.php
+++ src/app/Classes/Invoice.php
@@ -5,11 +5,11 @@ declare(strict_types=1);
 namespace App\Classes;
 
 class Invoice {
-    public static function index():string{
+    public function index():string{
         return 'INVOICES';
     }
 
-    public static function create():string{
+    public function create():string{
         return 'Create Invoice';
     }
 }
